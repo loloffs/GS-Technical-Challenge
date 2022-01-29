452e59f9c1468c35f1da466418577091a54a2a8f
loloffs <lukeoloffs@gmail.com>
2022-01-29 11:17:35 -0800

    Code cleanup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,39 +7,39 @@
 </template>
 
 <script>
-export default {
-  name: 'Navbar',
-  props: {
-    mainHeading: String,
-    subHeading: String,
-    logo: Image
+  export default {
+    name: 'Navbar',
+    props: {
+      mainHeading: String,
+      subHeading: String,
+      logo: Image
+    }
   }
-}
 </script>
 
 <style scoped>
-.navbar {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  background-color: #E0E0E0;
-  width: 100%;
-  top: 0;
-  z-index: 10000;
-  margin-bottom: .5em;
-}
-h1 {
-  margin: 10px 0 0 0;
-  color: black;
-}
-#logo {
-  width: 17em;
-  height: auto;
-  margin-top: 7em;
-}
-p {
-  color: black;
-}
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background-color: #E0E0E0;
+    width: 100%;
+    top: 0;
+    z-index: 10000;
+    margin-bottom: .5em;
+  }
+  h1 {
+    margin: 10px 0 0 0;
+    color: black;
+  }
+  #logo {
+    width: 17em;
+    height: auto;
+    margin-top: 7em;
+  }
+  p {
+    color: black;
+  }
 </style>
